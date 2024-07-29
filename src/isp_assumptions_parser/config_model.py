4da04259f29cc9a34209fc5b480d1ca47ce5f1d4
prakaa <abiprakash007@gmail.com>
2024-07-30 09:57:50 +1000

    add yaml loader function

--- src/isp_assumptions_parser/config_model.py
+++ src/isp_assumptions_parser/config_model.py
@@ -1,5 +1,8 @@
+import yaml
+
 from pydantic import BaseModel
 from typing import List
+from pathlib import Path
 
 
 class Table(BaseModel):
@@ -9,3 +12,11 @@ class Table(BaseModel):
     end_row: int
     column_range: str
     header_names: List[str]
+
+
+def load_yaml(path: Path) -> List[Table]:
+    with open(path, "r") as f:
+        config = yaml.safe_load(f)
+        f.close()
+    tables = [Table(name=name, **config[name]) for name in config]
+    return tables
