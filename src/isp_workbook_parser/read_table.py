b257dde4f3953751adc1e3fc9af7541b4c584d7e
prakaa <abiprakash007@gmail.com>
2024-10-10 09:02:38 +1100

    Refactor read_table

--- src/isp_workbook_parser/read_table.py
+++ src/isp_workbook_parser/read_table.py
@@ -1,11 +1,13 @@
-import openpyxl
+from typing import List, Union
 
+import numpy as np
+import openpyxl
 import openpyxl.utils
 import pandas as pd
-import numpy as np
 
 from isp_workbook_parser import TableConfig
-from typing import Union, List
+
+from .sanitisers import _column_name_sanitiser
 
 
 def read_table(workbook_file: pd.ExcelFile, table: TableConfig) -> pd.DataFrame:
@@ -62,131 +64,6 @@ def read_table(workbook_file: pd.ExcelFile, table: TableConfig) -> pd.DataFrame:
     Returns:
         Table as a pandas DataFrame
     """
-
-    def _column_name_sanitiser(columns: pd.Index | pd.Series) -> pd.Index | pd.Series:
-        """
-        Sanitises column names by:
-        1. Removing 'versioning' from column names introduced by `mangle_dupe_cols` in
-        pandas parser, e.g. 'Generator.1' is sanitised to 'Generator'
-        2. Stripping leading and trailing whitespaces
-        3. Remove any newlines
-        4. Removes trailing numbers that are footnotes
-        """
-        columns = columns.astype(str)
-        columns = columns.str.replace(r"\.[\.\d]+$", "", regex=True)
-        columns = columns.str.strip()
-        columns = columns.str.replace(r"\n", r" ", regex=True)
-        columns = columns.str.replace(r"(?<=[^\s\d])\d$", "", regex=True)
-        return columns
-
-    def _ffill_highest_header(initial_header: pd.Series) -> pd.Series:
-        """
-        Forward fills the highest header row (parsed as DataFrame columns) for processing
-        a multi-header table
-        """
-        initial_header[initial_header.str.contains("Unnamed")] = pd.NA
-        ffill_initial_header = initial_header.ffill().reset_index(drop=True).fillna("")
-        return ffill_initial_header
-
-    def _ffill_intermediate_header_row(
-        intermediate_header: pd.Series, preceding_header: pd.Series
-    ) -> pd.Series:
-        """
-        Forward fills intermediate header row (parsed as a DataFrame row), with the
-        following strategy:
-
-        1. If the nth element value of the intermediate header is NaN, make the
-        nth element equal to the (n-1)th value in the intermediate header row if the
-        nth element in the preceding header is NaN.
-        2. If the nth element value is equal to the nth value of the preceding header,
-        (e.g. "Name" in row 1 and "Name" in row 2), then make the nth element value
-        NaN
-
-        N.B. "Unnamed" columns in pandas are actually NaNs
-        """
-        int_header = intermediate_header.copy(deep=True)
-        for n, value in zip(range(1, len(int_header)), int_header.iloc[1:]):
-            preceding_value = preceding_header.iloc[n]
-            if pd.isna(value):
-                if pd.isna(preceding_value):
-                    int_header.iloc[n] = int_header.iloc[n - 1]
-            elif not pd.isna(preceding_value) and value == preceding_value:
-                int_header.iloc[n] = pd.NA
-
-        _ffill_intermediate_header = int_header.reset_index(drop=True).fillna("")
-        _ffill_intermediate_header = _column_name_sanitiser(_ffill_intermediate_header)
-        return _ffill_intermediate_header
-
-    def _process_last_header_row(
-        last_header: pd.Series, preceding_header: pd.Series
-    ) -> pd.Series:
-        """
-        Processes last header row by removing duplicated table names if the nth element
-        value is equal to the nth value of the preceding header,
-        (e.g. "Name" in row 1 and "Name" in row 2).
-        This is done by making the nth element value an empty string
-        """
-        last_header = last_header.reset_index(drop=True).fillna("")
-        last_header = _column_name_sanitiser(last_header)
-        last_header = last_header.where(last_header != preceding_header, "")
-        return last_header
-
-    def _build_cleaned_dataframe(
-        df_initial: pd.DataFrame,
-        header_rows_in_table: int,
-        new_headers: pd.Series,
-        forward_fill_values: bool,
-    ) -> pd.DataFrame:
-        """
-        Builds a cleaned DataFrame with the merged headers by:
-        1. Dropping the header rows in the table
-        2. Applying the merged headers as the columns of the DataFrame
-        3. Forward fill values across columns if `forward_fill_values` is True
-        4. Reset the DataFrame index
-        """
-        df_cleaned = df_initial.iloc[header_rows_in_table:, :]
-        df_cleaned.columns = new_headers
-        if forward_fill_values:
-            df_cleaned = df_cleaned.ffill(axis=1)
-        df_cleaned = df_cleaned.reset_index(drop=True)
-        return df_cleaned
-
-    def _skip_rows_in_dataframe(
-        df: pd.DataFrame, config_skip_rows: Union[int, List[int]], last_header_row: int
-    ) -> pd.DataFrame:
-        """
-        Drop rows specified by `skip_rows` by applying an offset from the header and
-        dropping based on index values
-        """
-        df_reset_index = df.reset_index(drop=True)
-        if isinstance(config_skip_rows, int):
-            skip_rows = [config_skip_rows - last_header_row - 1]
-        else:
-            skip_rows = np.subtract(config_skip_rows, last_header_row + 1)
-        dropped = df_reset_index.drop(index=skip_rows).reset_index(drop=True)
-        return dropped
-
-    def _handle_merged_rows(
-        df: pd.DataFrame,
-        config_cols_with_merged_rows: Union[str, List[str]],
-        column_range: str,
-    ) -> pd.DataFrame:
-        """
-        Forward fill down columns in `columns_with_merged_rows`
-        """
-        if type(config_cols_with_merged_rows) is str:
-            cols = [config_cols_with_merged_rows]
-        else:
-            cols = config_cols_with_merged_rows
-        absolute_col_indices = list(map(openpyxl.utils.column_index_from_string, cols))
-        first_col_index = openpyxl.utils.column_index_from_string(
-            column_range.split(":")[0]
-        )
-        actual_col_indices = [x - first_col_index for x in absolute_col_indices]
-        for index in actual_col_indices:
-            df.iloc[:, index] = df.iloc[:, index].ffill()
-        return df
-
     if type(table.header_rows) is int:
         df = pd.read_excel(
             workbook_file,
@@ -257,3 +134,137 @@ def read_table(workbook_file: pd.ExcelFile, table: TableConfig) -> pd.DataFrame:
                 df_cleaned, table.columns_with_merged_rows, table.column_range
             )
         return df_cleaned
+
+
+def _ffill_highest_header(initial_header: pd.Series) -> pd.Series:
+    """
+    Forward fills the highest header row (parsed as DataFrame columns) for processing
+    a multi-header table
+    """
+    initial_header[initial_header.str.contains("Unnamed")] = pd.NA
+    ffill_initial_header = initial_header.ffill().reset_index(drop=True).fillna("")
+    return ffill_initial_header
+
+
+def _ffill_intermediate_header_row(
+    intermediate_header: pd.Series, preceding_header: pd.Series
+) -> pd.Series:
+    """
+    Forward fills intermediate header row (parsed as a DataFrame row), with the
+    following strategy:
+
+    1. If the nth element value of the intermediate header is NaN, make the
+    nth element equal to the (n-1)th value in the intermediate header row if the
+    nth element in the preceding header is NaN.
+    2. If the nth element value is equal to the nth value of the preceding header,
+    (e.g. "Name" in row 1 and "Name" in row 2), then make the nth element value
+    NaN
+
+    N.B. "Unnamed" columns in pandas are actually NaNs
+    """
+    int_header = intermediate_header.copy(deep=True)
+    for n, value in zip(range(1, len(int_header)), int_header.iloc[1:]):
+        preceding_value = preceding_header.iloc[n]
+        if pd.isna(value):
+            if pd.isna(preceding_value):
+                int_header.iloc[n] = int_header.iloc[n - 1]
+        elif not pd.isna(preceding_value) and value == preceding_value:
+            int_header.iloc[n] = pd.NA
+
+    _ffill_intermediate_header = int_header.reset_index(drop=True).fillna("")
+    _ffill_intermediate_header = _column_name_sanitiser(_ffill_intermediate_header)
+    return _ffill_intermediate_header
+
+
+def _process_last_header_row(
+    last_header: pd.Series, preceding_header: pd.Series
+) -> pd.Series:
+    """
+    Processes last header row by removing duplicated table names if the nth element
+    value is equal to the nth value of the preceding header,
+    (e.g. "Name" in row 1 and "Name" in row 2).
+    This is done by making the nth element value an empty string
+    """
+    last_header = last_header.reset_index(drop=True).fillna("")
+    last_header = _column_name_sanitiser(last_header)
+    last_header = last_header.where(last_header != preceding_header, "")
+    return last_header
+
+
+def _build_cleaned_dataframe(
+    df_initial: pd.DataFrame,
+    header_rows_in_table: int,
+    new_headers: pd.Series,
+    forward_fill_values: bool,
+) -> pd.DataFrame:
+    """
+    Builds a cleaned DataFrame with the merged headers by:
+    1. Dropping the header rows in the table
+    2. Applying the merged headers as the columns of the DataFrame
+    3. Forward fill values across columns if `forward_fill_values` is True
+    4. Reset the DataFrame index
+    """
+    df_cleaned = df_initial.iloc[header_rows_in_table:, :]
+    df_cleaned.columns = new_headers
+    if forward_fill_values:
+        df_cleaned = df_cleaned.ffill(axis=1)
+    df_cleaned = df_cleaned.reset_index(drop=True)
+    return df_cleaned
+
+
+def _skip_rows_in_dataframe(
+    df: pd.DataFrame, config_skip_rows: Union[int, List[int]], last_header_row: int
+) -> pd.DataFrame:
+    """
+    Drop rows specified by `skip_rows` by applying an offset from the header and
+    dropping based on index values
+    """
+    df_reset_index = df.reset_index(drop=True)
+    if isinstance(config_skip_rows, int):
+        skip_rows = [config_skip_rows - last_header_row - 1]
+    else:
+        skip_rows = np.subtract(config_skip_rows, last_header_row + 1)
+    dropped = df_reset_index.drop(index=skip_rows).reset_index(drop=True)
+    return dropped
+
+
+def _handle_merged_rows(
+    df: pd.DataFrame,
+    config_cols_with_merged_rows: Union[str, List[str]],
+    column_range: str,
+) -> pd.DataFrame:
+    """
+    Forward fill down columns in `columns_with_merged_rows`
+    """
+    if type(config_cols_with_merged_rows) is str:
+        cols = [config_cols_with_merged_rows]
+    else:
+        cols = config_cols_with_merged_rows
+    actual_col_indices = list(
+        map(lambda col: _find_data_column_index(col, column_range), cols)
+    )
+    for index in actual_col_indices:
+        df.iloc[:, index] = df.iloc[:, index].ffill()
+    return df
+
+
+def _find_data_column_index(
+    column_alphabetical: str, column_range_from_table_config: str
+) -> int:
+    """Returns the zero-index (integer) index of a column within a table defined by
+    a TableConfig column range.
+
+    Args:
+        column_alphabetical: Alphabetical column index (e.g. "B")
+        column_range_from_table_config: Alphabetical column range, as defined in each
+            TableConfig (e.g. "B:D")
+
+    Returns:
+        Integer index of the column that `column_alphabetical` refers to in the data
+        (zero-indexed)
+    """
+    first_col_index = openpyxl.utils.column_index_from_string(
+        column_range_from_table_config.split(":")[0]
+    )
+    data_col_index = openpyxl.utils.column_index_from_string(column_alphabetical)
+    return data_col_index - first_col_index
